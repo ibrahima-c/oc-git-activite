c4b4d02b2bc15b65ab2145912384486d6c9a47df
Ibrahima Camara <ibrahima.s.camara@gmail.com>
2018-08-21 19:31:27 +0200

    ajout du fichier TestUtils.java

---
+++ src/com/oc/utils/test/TestUtils.java
@@ -0,0 +1,24 @@
+package com.oc.utils.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.oc.utils.Utils;
+
+public class TestUtils {
+
+    public static void main( String[] args ) {
+
+        List<Double> list = new ArrayList<Double>();
+
+        list.add( 0.0 );
+        list.add( 10.0 );
+        list.add( 15.0 );
+
+        Double max = Utils.findMax( list );
+
+        System.out.println( max );
+
+    }
+
+}
